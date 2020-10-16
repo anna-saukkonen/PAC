c97291ceaa7a0c40e47e80db74c6af50f1fc2734
anna-saukkonen <67560494+anna-saukkonen@users.noreply.github.com>
2020-10-16 10:14:51 +0100

    Add files via upload

---
+++ bin/paternal_extra.pl
@@ -0,0 +1,1 @@
+open (F,"paternal_tags_UM.txt"); my %tag=(); while (<F>) { chomp; $tag{$_}=0; } close (F); open (N,"paternal_tags_UM.RSEM.txt"); while (<N>) { chomp; if (!(exists $tag{$_})) { print "$_\n"; }} close (N);
