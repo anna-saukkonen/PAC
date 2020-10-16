04f62319985058b708a0475aa98d6197ee797c20
anna-saukkonen <67560494+anna-saukkonen@users.noreply.github.com>
2020-10-16 10:10:33 +0100

    Add files via upload

---
+++ bin/maternal_extra.pl
@@ -0,0 +1,1 @@
+open (F,"maternal_tags_UM.txt"); my %tag=(); while (<F>) { chomp; $tag{$_}=0; } close (F); open (N,"maternal_tags_UM.RSEM.txt"); while (<N>) { chomp; if (!(exists $tag{$_})) { print "$_\n"; }} close (N);
