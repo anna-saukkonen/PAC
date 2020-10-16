01ee02797e67eeb1f5a05ef088a76af8eaeb8dd1
anna-saukkonen <67560494+anna-saukkonen@users.noreply.github.com>
2020-10-16 15:51:00 +0100

    Update paternal_extra.pl

--- bin/paternal_extra.pl
+++ bin/paternal_extra.pl
@@ -1,3 +1,3 @@
 #!/usr/bin/perl
 
-open (F,"paternal_tags_UM.txt"); my %tag=(); while (<F>) { chomp; $tag{$_}=0; } close (F); open (N,"paternal_tags_UM.RSEM.txt"); while (<N>) { chomp; if (!(exists $tag{$_})) { print "$_\n"; }} close (N);
+open (F,"paternal_tags_UM.txt") or die $!; my %tag=(); while (<F>) { chomp; $tag{$}=0; } close (F); open (N,"paternal_tags_UM.RSEM.txt") or die $!; while (<N>) { chomp; if (!(exists $tag{$})) { print "$_\n"; }} close (N);
