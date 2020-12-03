1f01f5e9dcfb68ac8c1ec37511e0f376e2c8e89e
anna-saukkonen <67560494+anna-saukkonen@users.noreply.github.com>
2020-12-03 16:14:16 +0000

    Delete paternal_extra.pl

--- bin/paternal_extra.pl
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/perl
-
-open (F,"paternal_tags_UM.txt") or die $!; my %tag=(); while (<F>) { chomp; $tag{$}=0; } close (F); open (N,"paternal_tags_UM.RSEM.txt") or die $!; while (<N>) { chomp; if (!(exists $tag{$})) { print "$_\n"; }} close (N);
